078ae3c717f8647face2612ef4505710d9ba3c6f
unknown <sh.51to.21@gmail.com>
2024-07-08 10:41:25 +0900

    semonster2の開発(1)

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -11,4 +11,10 @@ public class AppTest {
         App classUnderTest = new App();
         assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
+    @Test
+    public void apptest() {
+      App classUnderTest = new App();
+      assertEquals("こんにちは SEMonster", 
+      classUnderTest.getGreeting());
+    }
 }
